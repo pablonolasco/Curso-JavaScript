95271c39329e46c85f47057f387bd7d4f1c0a9a6
pablonolasco <pablo.nolasco@live.com.mx>
2018-04-16 15:43:30 -0500

    Agregando archivos
    agregar nuevos archivos js

---
+++ 1primerosPasos/app.js
@@ -0,0 +1,5 @@
+let nombre=prompt('Cual es tu nombre:');
+
+document.getElementById('contenido').innerHTML=`El nombre es ${nombre}`;
+//console.warn("metodo warning");
+document.querySelector("h1");
